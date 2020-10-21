6b149df4f3f1b4aca9ab952e16db172c1425493b
acse-jtb20 <jtb20@ic.ac.uk>
2020-10-21 13:33:10 +0100

    added sin and tests

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,7 +1,8 @@
 from functools import lru_cache
 
-__all__ = ['my_sum', 'factorial']
+__all__ = ['my_sum', 'factorial','sin_series']
 
+#comment
 
 def my_sum(iterable):
     tot = 0
@@ -10,6 +11,20 @@ def my_sum(iterable):
     return tot
 
 
+
+
 @lru_cache(maxsize=None)  # Note: -> @cache in python >= 3.9
 def factorial(n):
     return n * factorial(n-1) if n else 1
+
+
+
+def calc_part(x,n):
+    return (x**(2*n+1))*((-1)**n)/(factorial(2*n+1))
+
+
+def sin_series(x):
+    lsit_series=[calc_part(x,n) for n in range(50)]
+    return my_sum(lsit_series)
+    
+
